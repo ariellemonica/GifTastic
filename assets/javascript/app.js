8d6eaa63a630d70ace8099d189cd2d9f6cf68b34
Monica Nadal <missmonicanadal@gmail.com>
2020-03-16 22:39:33 -0700

    20200316 started the array and did some pseudocode for next steps

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+//Here we begin the array of default button options
+$(document).ready(function(){
+    var topics = [excited, frustrated, ecstatic, angry, indignant, LOL];
+
+    //write a for loop that loops through the topics array and creates buttons
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
